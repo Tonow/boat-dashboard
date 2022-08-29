Fix speed and distance computation

data_computations.py
@@ -1,4 +1,5 @@
 import numpy as np
+from config import M_TO_MNI
 
 def get_point_dist(df):
     r = 6371000 # meters
@@ -17,12 +18,13 @@ def get_point_dist(df):
     return df
 
 def get_speed(df):
-    df['speed'] = df['arclength'] / (df.time.diff()  / np.timedelta64(1, 'ns'))
+    df['speed'] = df['distance'] * 2 * (M_TO_MNI * 1000)  / (df.time.diff()  / np.timedelta64(1, 's'))
     return df
 
 def get_abs_speed(df):
     df['speed'] = df['speed'].fillna(0)
-    df['abs_speed'] = np.abs(df['speed']).astype("int8")
+    # df['abs_speed'] = np.abs(df['speed']).astype("float32")
+    df['abs_speed'] = np.abs(df['speed'])
     return df
 
 def get_speeds(df):

get_data.py
@@ -28,6 +28,7 @@ class DateTimeEncoder(json.JSONEncoder):
 #     return data
 
 def compute_data(data):
+    data[DATE_COLUMN] = data[DATE_COLUMN].astype('float64')
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN], unit='ns')
     data[DATE_COLUMN] = data[DATE_COLUMN].astype('datetime64[s]')
     data = data.sort_values(by=DATE_COLUMN)
@@ -38,31 +39,25 @@ def compute_data(data):
 #     data = load_data()
 #     return compute_data(data)
 
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 20 min.
+@st.cache(ttl=1200)
+def run_query(query, conn):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
+@st.experimental_memo
 def get_data_from_gs_sheet():
     # Create a connection object.
-    conn = connect()
-
-    # Perform SQL query on the Google Sheet.
-    # Uses st.cache to only rerun when the query changes or after 10 min.
-    @st.cache(ttl=600)
-    def run_query(query):
-        rows = conn.execute(query, headers=1)
-        rows = rows.fetchall()
-        return rows
 
     sheet_url = st.secrets["public_gsheets_url"]
-    rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-    # df = pd.DataFrame({'col':L})
-    # st.write(rows)
-    # st.write(type(rows))
+    with connect() as conn:
+        rows = run_query(f'SELECT * FROM "{sheet_url}"', conn=conn)
 
     df = pd.read_json(json.dumps(rows, cls=DateTimeEncoder), orient='values')
     df.columns = COLUMNS
-    df["latitude"] = df["latitude"].astype(np.float16)
-    df["longitude"] = df["longitude"].astype(np.float16)
-    # st.write(pd.api.types.is_float_dtype(df["latitude"]))
-    # st.write(pd.api.types.is_float_dtype(df["longitude"]))
+    df["latitude"] = df["latitude"].astype(np.float32)
+    df["longitude"] = df["longitude"].astype(np.float32)
     df = compute_data(df)
     return df

⛵_boat_track.py
@@ -1,19 +1,15 @@
+from dataclasses import field
 from datetime import datetime
+from distutils.debug import DEBUG
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-from map_plot import get_gps_tracks, line_chart
-# from data_computations import get_speeds
-# from layers import layer2, layer_boat
-# from random_data import rand_lat_lon
+from map_plot import get_gps_tracks, line_chart, histogram_chart
 from get_data import get_data_from_gs_sheet
-
-
+from config import M_TO_MNI
 
 st.set_page_config(page_title="ToNo et les deux 🐈‍⬛", layout="wide", page_icon="⛵")
-
-
 
 
 # st.markdown(
@@ -29,7 +25,7 @@ st.set_page_config(page_title="ToNo et les deux 🐈‍⬛", layout="wide", page
 #     unsafe_allow_html=True,
 # )
 
-st.title("Boat tracks")
+st.title("Le chemin parcourue en ⛵")
 
 data = get_data_from_gs_sheet()
 
@@ -43,34 +39,43 @@ with st.sidebar:
         )
     )
 
-st.subheader(f"{start_time} : {end_time}")
+st.subheader(f"Du {start_time} au {end_time}")
 filtered_data = data[(data.index > start_time) & (data.index < end_time)]
 
 
-if st.checkbox('Show data'):
+if st.checkbox('Voir le tableau de données'):
     st.dataframe(filtered_data)
 
 fig = get_gps_tracks(filtered_data)
 st.plotly_chart(fig, use_container_width=True)
 
-col1, col2 = st.columns(2)
-col1.metric(label="Last speed", value=f"{filtered_data.speed[-1]:.1f} Knt/h", delta=f"{(filtered_data.speed[-1] - filtered_data.speed[-2]):.1f} Knt/h")
-col2.metric(label="Last Distance", value=f"{filtered_data.distance[-1]:.1f} Knt", delta=f"{(filtered_data.distance[-1] - filtered_data.distance[-2]):.1f} Knt")
+col1, col2, col3 = st.columns(3)
+col1.metric(label="Derrière vitesse", value=f"{filtered_data.speed[-1]:.1f} Knt/h", delta=f"{(filtered_data.speed[-1] - filtered_data.speed[-2]):.1f} Knt/h")
+col2.metric(label="Derrière distance", value=f"{filtered_data.distance[-1]/1000:.2f} Km", delta=f"{(filtered_data.distance[-1] - filtered_data.distance[-2])/1000:.2f} Km")
+col3.metric(label="Mille marin", value=f"{filtered_data.distance[-1] * M_TO_MNI:.2f} Mille nautique", delta=f"{(filtered_data.distance[-1] - filtered_data.distance[-2]) * M_TO_MNI:.2f} Mni")
 
 col11, col12= st.columns(2)
 
 with col11:
-    st.subheader('The absolute speed')
+    st.subheader('Vitesse absolue')
     st.plotly_chart(line_chart(filtered_data, "abs_speed"), use_container_width=True)
 
 with col12:
-    st.subheader('Distance')
-    st.plotly_chart(line_chart(filtered_data, "distance"), use_container_width=True)
+    field_dst = "distance"
+    st.subheader('Distance en Km')
+    distance_in_m = filtered_data.copy()
+    distance_in_m[field_dst] = filtered_data[field_dst] / 1000
+    st.plotly_chart(line_chart(distance_in_m, field_dst), use_container_width=True)
 
 
 col21, col22= st.columns(2)
 
-with col21:
-    st.subheader('Number of point by hour')
-    hist_values = np.histogram(filtered_data.index.hour, bins=24, range=(0,24))[0]
-    st.bar_chart(hist_values)
+if DEBUG:
+    with col21:
+        st.subheader('Number of point by hour')
+        hist_values = np.histogram(filtered_data.index.hour, bins=24, range=(0,24))[0]
+        st.bar_chart(hist_values)
+
+
+st.subheader('Distribution de la vitesse absolut')
+st.plotly_chart(histogram_chart(filtered_data, "abs_speed"), use_container_width=True)